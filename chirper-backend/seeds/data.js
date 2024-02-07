0ea83cbb92241bae01e0045c308d1e9db535d5c6
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-02-07 22:41:15 +1100

    Fixed seed script

--- chirper-backend/seeds/data.js
+++ chirper-backend/seeds/data.js
@@ -3,40 +3,40 @@
  * @returns { Promise<void> } 
  */
 exports.seed = async function(knex) {
-  const info = await knex.table("sqlite_master").select();
-  console.info(info);
+  console.info("Deleting ALL existing data...");
 
   await knex("users").del();
   await knex("chirps").del();
   await knex("follows").del();
   await knex("profiles").del()
 
-  const userIds = await knex.table("users").insert(
+  console.info("Deleted ALL existing data");
+
+  const userIds = (await knex.table("users").insert(
     [{ email: "alice@alice.com", username: "alice", password: "password" },
     { email: "bob@bob.com", username: "bob", password: "password" },
     { email: "charlie@charlie.com", username: "charlie", password: "password" },
     { email: "duck@duck.com", username: "duck", password: "password" }]
-  ).returning("id");
+  ).returning("id")).map(row => row.id);
 
-  userIds.forEach((id) => {
+  console.info("Inserted users for ids:", userIds);
+  
+  await Promise.all(userIds.map((id) => 
       knex.table("chirps").insert(
           [{ user_id: id, text_content: "Hello, world!" },
           { user_id: id, text_content: "Goodbye, world!" }]
-      ).then(result => console.info(`${result} chirps inserted`));
-  });
+      ).then(result => console.info(`${result} chirps inserted`))
+  ));
 
-  knex.table("users").select().then(result => console.log(result));
-
-
-  userIds.slice(0, userIds.length - 1).forEach((id, index, array) => {
+  await Promise.all(userIds.slice(0, userIds.length - 1).map((id, index, array) => 
       knex.table("follows").insert(
           { follower_id: id, followee_id: array[index + 1] },
-      ).then(result => console.info(`${result} follows inserted`));
-  });
+      ).then(result => console.info(`${result} follows inserted`))
+  ));
 
-  userIds.forEach((id) => {
+  await Promise.all(userIds.map((id) => 
       knex.table("profiles").insert(
           { user_id: id, bio: "I am a person." }
-      ).then(result => console.info(`${result} profiles inserted`));
-  });
+      ).then(result => console.info(`${result} profiles inserted`))
+  ));
 };
