315186006ea01f758b44a2f4f1cd64e4641485be
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-02-07 15:05:53 +1100

    Setup typescript and base express app in backend

---
+++ chirper-backend/src/index.ts
@@ -0,0 +1,12 @@
+import app from './app';
+import { Request, Response } from 'express';
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (_: Request, res: Response) => {
+    res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
