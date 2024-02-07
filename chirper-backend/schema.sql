212909e2a1093335cab1fc354488d906336c81ae
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-02-07 16:37:41 +1100

    Added mock data for dev purposes

--- chirper-backend/schema.sql
+++ chirper-backend/schema.sql
@@ -1,19 +1,29 @@
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    email TEXT,
+    password TEXT
+);
+
 CREATE TABLE chirps (
-  id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id),
-  text_content TEXT,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    text_content TEXT,
+    created_at TIMESTAMP
 );
 
 CREATE TABLE profiles (
-  id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id),
-  bio TEXT,
-  avatar_url TEXT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    bio TEXT,
+    avatar_url TEXT
 );
 
 CREATE TABLE follows (
-  follower_id INTEGER REFERENCES users(id),
-  followee_id INTEGER REFERENCES users(id),
-  PRIMARY KEY (follower_id, followee_id)
+    follower_id INTEGER,
+    followee_id INTEGER,
+    PRIMARY KEY (follower_id, followee_id),
+    FOREIGN KEY (follower_id) REFERENCES users(id),
+    FOREIGN KEY (followee_id) REFERENCES users(id)
 );
